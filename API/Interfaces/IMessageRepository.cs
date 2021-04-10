deaef661177ee92b6f2a5db7058d72941a78c5e4
Taylor <tbuchhei@gmail.com>
2021-04-10 16:26:37 -0700

    End of Section 18

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -19,6 +19,5 @@ namespace API.Interfaces
         Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams); 
         Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, 
             string recipientUsername);
-        Task<bool> SaveAllAsync();
     }
 }
